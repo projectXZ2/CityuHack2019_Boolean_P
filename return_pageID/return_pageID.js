7b58f906763f205ec0dc2939359fd701e15839f6
kam <kamkam6b@hotmail.com>
2019-01-26 19:01:38 +0800

    .

--- return_pageID/return_pageID.js
+++ return_pageID/return_pageID.js
@@ -1,5 +1,45 @@
-alert("Hello");
+var path = "https://api.myjson.com/bins/ij0xc";
+var str = "";
+getJSON(path);
 
-var json = $.getJSON("6_tagged.json");
-var data = eval("(" +json.responseText + ")");
-document.write(data["a"]);
+function getJSON(path){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        // console.log(myObj);
+        var strJSON = JSON.stringify(myObj);
+        document.getElementById("json").innerHTML = strJSON;
+        str = strJSON;
+        jsonStr = myObj;
+        // console.log(myObj);
+      }
+    };
+    xmlhttp.open("GET", path, true);
+    xmlhttp.send();
+}
+
+function search(){
+    var type = document.getElementById("textInput").value;
+    console.log("type: " + type);
+
+    // var jsonStr = document.getElementById("json").innerHTML;
+    var jsonStr = str;
+    var jsonObj = JSON.parse(jsonStr);
+    var pages = jsonObj.pages;
+
+    for(i=0; i<pages.length; i++){
+        if(pages[i].tags.length > 0){
+            var page = pages[i];
+            var tags = page.tags;
+            for(j=0; j<tags.length; j++){
+                var tag = tags[j];
+                var entityType = tag.entityType;
+                if(entityType == type){
+                    console.log(pages[i].pageId);
+                }
+            }
+        }
+    }
+
+}
